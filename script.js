fcbbab09734a6608902c0a728bd8b31f0d1c4ad5
Ol-Hl07 <san4ogl10@gmail.com>
2026-02-04 18:49:17 +0200

    Add rodLevel functionality for fishing game

--- script.js
+++ script.js
@@ -6,16 +6,43 @@ let level = localStorage.getItem("level")
   ? parseInt(localStorage.getItem("level"))
   : 1;
 
+let rodLevel = localStorage.getItem("rodLevel")
+  ? parseInt(localStorage.getItem("rodLevel"))
+  : 1;
+
 function catchFish() {
-  fish++;
+  fish += rodLevel;
 
   if (fish >= level * 5) {
     level++;
   }
 
+  save();
+  render();
+}
+
+function buyRod() {
+  if (fish >= 10) {
+    fish -= 10;
+    rodLevel++;
+    save();
+    render();
+  } else {
+    alert("❌ Недостатньо риби");
+  }
+}
+
+function save() {
   localStorage.setItem("fish", fish);
   localStorage.setItem("level", level);
-
-  document.getElementById("result").innerText =
-    "🐟 Риба: " + fish + " | ⭐ Рівень: " + level;
+  localStorage.setItem("rodLevel", rodLevel);
 }
+
+function render() {
+  document.getElementById("result").innerText =
+    "🐟 Риба: " + fish +
+    " | ⭐ Рівень: " + level +
+    " | 🎣 Вудка: " + rodLevel;
+}
+
+render();
